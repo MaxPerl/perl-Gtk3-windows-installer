111227737b2743763aefca20927df80e90d8d24d
MaxPerl <max.augsburg@gmx.de>
2017-06-08 13:07:23 +0200

    Version 0.03
    Add options -d for destdir and -n for non-interactive mode

--- install-perl-Gtk3-mingw64.pl
+++ install-perl-Gtk3-mingw64.pl
@@ -6,20 +6,23 @@ use Cwd;
 use File::Path;
 use POSIX qw(:sys_wait_h);
 use Data::Dumper;
+use Getopt::Std;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 select((select(STDERR),$| = 1)[0]);
 select((select(STDOUT),$| = 1)[0]);
 
 die "Wrong OS. Did you start the script from the MSys2 Terminal? Please use the MinGW64 Terminal. Thanks!\n" unless ($^O eq 'MSWin32');
 
+our($opt_n, $opt_d);
+getopts('nd:');
+
 my $script_dir = cwd();
 my $build_dir = "$script_dir/perl-Gtk3-Build";
 mkdir("$build_dir") or die "Could not create the build directory: $! \n Please remove $script_dir/perl-Gtk3-Build with rm -R \n";
 
 print_welcome_page();
-
 
 print "*----------------------------------------\n";
 print "* INSTALLING THE BUILD TOOLCHAIN \n";
@@ -32,7 +35,13 @@ print " [OK]\n\n";
 print "*----------------------------------------\n";
 print "* INSTALLING NATIVE DEPENDENCIES \n";
 print "*----------------------------------------\n";
-$exitcode = exec_with_progress("pacman -S --needed --noconfirm mingw-w64-x86_64-gobject-introspection mingw-w64-x86_64-cairo mingw-w64-x86_64-gtk3 >> $script_dir/install.log 2>> $script_dir/error.log");
+my $exitcode;
+if ($opt_d) {
+	$exitcode = exec_with_progress("pacman -S --needed --noconfirm --root $opt_d mingw-w64-x86_64-gobject-introspection mingw-w64-x86_64-cairo mingw-w64-x86_64-gtk3 >> $script_dir/install.log 2>> $script_dir/error.log");
+}
+else {
+	$exitcode = exec_with_progress("pacman -S --needed --noconfirm mingw-w64-x86_64-gobject-introspection mingw-w64-x86_64-cairo mingw-w64-x86_64-gtk3 >> $script_dir/install.log 2>> $script_dir/error.log");
+}
 die "[ FAIL]\n", $exitcode/256, ": ERROR at installing native dependencies: $!\n" if ($exitcode != 0);
 print "[ OK]\n\n";
 
@@ -52,23 +61,70 @@ $exitcode = exec_with_progress("curl -s -L https://cpanmin.us | perl - App::cpan
 die " [FAIL]\n", $exitcode/256, ": ERROR at installing App::cpanminus: $!\n" if ($exitcode != 0);
 print " [OK]\n";
 
-print "Installing ExtUtils::Depends and ExtUtils::PkgConfig";
-$exitcode = exec_with_progress("cpanm ExtUtils::Depends ExtUtils::PkgConfig >> $script_dir/install.log 2>> $script_dir/error.log");
-die " [FAIL]\n", $exitcode/256, ": ERROR at installing Perl Dependencies: $!\n" if ($exitcode != 0);
-print " [OK]\n\n";
+install_perl_module('ExtUtils::Depends');
+install_perl_module('ExtUtils::PkgConfig');
 
 install_prereq_module('Glib');
 install_prereq_module('Cairo');
 install_prereq_module('Glib::Object::Introspection');
 install_prereq_module('Cairo::GObject');
 
-print "*----------------------------------------\n";
-print "* INSTALLING PERL/Gtk3 \n";
-print "*----------------------------------------\n";
-$exitcode = exec_with_progress("cpanm -n Gtk3 >> $script_dir/install.log 2>> $script_dir/error.log");
-die " [FAIL]\n", $exitcode/256, ": ERROR at installing Gtk3: $!\n" if ($exitcode != 0);
-print " [OK]\n";
+install_perl_module('Gtk3');
 
+exit_cleanly();
+
+
+#########################
+# Functions
+#########################
+sub install_perl_module {
+	my ($module) = @_;
+	
+	print "*--------------------------\n";
+	print "* INSTALLING \U$module \n";
+	print "*--------------------------\n";
+	
+	my $info = `cpanm --info $module`;
+	#$info =~ s/\s*$//;
+	chomp $info;
+	my $link = get_link($info);
+
+	print "Download $module";
+	chdir("$build_dir");
+	$exitcode = system("wget -q $link >> $script_dir/install.log 2>> $script_dir/error.log");
+	die "    [FAIL]\n", $exitcode/256, ": ERROR: Could not download $module: $!\n" if ($exitcode != 0);
+	print "    [OK]\n";
+	
+	print "Extract $module";
+	my (undef, $filename) = $info =~ m/(.*)\/(.*)/;
+	my $dirname = $filename;
+	$dirname =~ s/\.tar\.gz$//;
+	$exitcode = system("tar xzf $filename >> $script_dir/install.log 2>> $script_dir/error.log");
+	die "    [FAIL]\n", $exitcode/256, ": ERROR: Could not extract $filename: $!\n" if ($exitcode != 0);
+	print "    [OK]\n";
+
+	print "perl Makefile.PL";
+	chdir("$build_dir/$dirname") or die "Could not change to $build_dir/$dirname\n";
+	my $exitcode;
+	if ($opt_d) {
+		$exitcode = exec_with_progress("perl ./Makefile.PL DESTDIR=\"$opt_d\" INSTALLDIRS=\"vendor\" INSTALLVENDORARCH=\"mingw64/lib/perl5/vendor_perl\"  INSTALLVENDORBIN=\"mingw64/bin\" INSTALLVENDORLIB=\"mingw64/lib/perl5/vendor_perl\" NO_PERLLOCAL=1 >> $script_dir/install.log 2>> $script_dir/error.log");
+	}
+	else {
+		$exitcode = exec_with_progress("perl ./Makefile.PL >> $script_dir/install.log 2>> $script_dir/error.log");
+	}	
+	die " [FAIL]\n", $exitcode/256, ": ERROR: Could not build $module: $!\n" if ($exitcode != 0);
+	print " [OK]\n";
+
+	print "Building $module";
+	$exitcode = exec_with_progress("dmake >> $script_dir/install.log 2>> $script_dir/error.log");
+	die " [FAIL]\n", $exitcode/256, ": ERROR: Could not build $module: $!\n" if ($exitcode != 0);
+	print " [OK]\n";
+		
+	print "Installing $module";
+	$exitcode = exec_with_progress("dmake install >> $script_dir/install.log 2>> $script_dir/error.log");
+	die " [FAIL]\n", $exitcode/256, ": ERROR: Could not install $module: $!\n" if ($exitcode != 0);
+	print " [OK]\n\n";
+}
 
 sub install_prereq_module {
 	my ($module) = @_;
@@ -150,7 +206,14 @@ sub make_file_hack {
 		print "LIBS: $element\n";
 	}
 
-	system("perl ./Makefile.PL LIBS=\"@libs\" >> $script_dir/install.log 2>> $script_dir/error.log");
+	#system("perl ./Makefile.PL LIBS=\"@libs\" >> $script_dir/install.log 2>> $script_dir/error.log");
+	my $exitcode;
+	if ($opt_d) {
+		system("perl ./Makefile.PL LIBS=\"@libs\" DESTDIR=\"$opt_d\" INSTALLDIRS=\"vendor\" INSTALLVENDORARCH=\"mingw64/lib/perl5/vendor_perl\"  INSTALLVENDORBIN=\"mingw64/bin\" INSTALLVENDORLIB=\"mingw64/lib/perl5/vendor_perl\" NO_PERLLOCAL=1 >> $script_dir/install.log 2>> $script_dir/error.log");
+	}
+	else {
+		system("perl ./Makefile.PL LIBS=\"@libs\" >> $script_dir/install.log 2>> $script_dir/error.log");
+	}
 }
 
 sub get_link {
@@ -213,23 +276,14 @@ sub print_welcome_page {
 
 	print "1) Download and install the installer from https://msys2.github.io/ \n";
 	print "2) Start the MinGW64 Terminal \n";
-	print "3) Run update-core \n";
+	print "3) Run pacman -Syu \n";
 	print "4) Close (X or Alt-4) and restart the MinGW64 Terminal \n";
-	print "5) Run pacman -Suu (and if needed/the program asked for close the terminal) \n\n";
-
-	print "NOTE that you can get the following warning: \n";
-	print "the shell starting scripts have been unified. Please update your shortcuts to the following targets, otherwise they will STOP WORKING:\n";
-	print "   \* MSYS2_ROOT\\msys2_shell.cmd -mingw32\n";
-	print "   \* MSYS2_ROOT\\msys2_shell.cmd -mingw64\n";
-	print "   \* MSYS2_ROOT\\msys2_shell.cmd -msys\n\n";
-
-	print "6) Therefore if needed update your shortcuts\n";
-	print "7) Repeat step 5 until nothing is to update anymore\n";
-	print "8) Install perl with pacman -S --needed --noconfirm mingw-w64-x86_64-perl\n\n";
+	print "5) Run pacman -Su (and if needed/the program asked for close the terminal) \n";
+	print "6) Install perl with pacman -S --needed --noconfirm mingw-w64-x86_64-perl\n\n";
 
 	print "If you followed these steps, press <ENTER> to start the installation!\n";
 	print "Otherwise exit the program with Ctrl-C!\n";
-	<STDIN>;
+	<STDIN> unless ($opt_n);
 }
 
 # With this function you can easyils install the perl module XML::Parser::Expat
@@ -287,8 +341,8 @@ sub install_xml_parser_expat {
 	print " [OK]\n\n";
 }
 
-#END {
-#	chdir($script_dir);
-#	print "Räume auf! \n";
-#	rmtree($build_dir);
-#}
+sub exit_cleanly {
+	chdir($script_dir);
+	print "Räume auf! \n";
+	rmtree($build_dir);
+}
